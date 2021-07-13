223cecc8040ba083db2e41d0312893775151e21c
Oladele Ogunsanya <33936524+oladele1440@users.noreply.github.com>
2021-07-13 15:31:14 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 # Input variable definitions
 
 variable  "access_key" {
-  default = "AKIA46SDTHMIUECMEBMZ"
+  default = ""
 } 
 
 variable "secret_key" {
-  default = "QGzC4L2HxdLp8di+n8lSmzcZaxzbzJz94/qbxD6w"
+  default = ""
 }
 
 variable "vpc_name" {
